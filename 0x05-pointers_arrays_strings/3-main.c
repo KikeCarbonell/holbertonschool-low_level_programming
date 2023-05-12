3d61e73e6ed80ba1cb17d68ff1cc36d9e8e10599
Kike <karbo77holberton@hotmail.com>
2023-05-12 15:33:30 -0500

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    str = "sere un gran programador";
+    _puts(str);
+    return (0);
+}
