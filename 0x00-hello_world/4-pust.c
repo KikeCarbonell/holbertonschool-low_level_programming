c44b447095b8715203403e8ba6f7dd5eaefcd1d1
Kike Carbonell <karbo77holberton@gmail.com>
2024-01-28 11:12:00 -0500

    4.Hello, puts

--- 0x00-hello_world/4-pust.c
+++ 0x00-hello_world/4-pust.c
@@ -1,9 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
-main()
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n
-");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
